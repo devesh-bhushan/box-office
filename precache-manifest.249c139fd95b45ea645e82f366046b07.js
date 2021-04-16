f708548dab06f5367b16db1d75ec5f3c6ad01f1e
Devesh <deepubhushan123@gmail.com>
2021-04-17 01:49:07 +0530

    Updates

---
+++ precache-manifest.249c139fd95b45ea645e82f366046b07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa4390962dad35220824bc610004918",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "da45ff01f41561400979",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "6369025ce425be61e59e",
+    "url": "/box-office/static/js/2.4ecbd64f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.4ecbd64f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da45ff01f41561400979",
+    "url": "/box-office/static/js/main.c70a21ca.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
